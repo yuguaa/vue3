d3f4618938e85a67aba15f5cba302622a6f00dc0
yuguaa <280722781@qq.com>
2022-12-06 09:29:53 +0800

    feat: vite plugin vue dynamic path

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue'
+import vitePluginVueDynamicPath from './vitePluginVueDynamicPath.js'
 
 const path = require('path')
+const VITE_PUBLIC_PATH = '/vue3-project/'
+const root = process.cwd()
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: VITE_PUBLIC_PATH,
+  root,
   server: {
     host: '0.0.0.0',
     port: 3000,
     open: true
   },
-  plugins: [vue()],
+  plugins: [vue(), vitePluginVueDynamicPath()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -29,5 +33,47 @@ export default defineConfig({
         javascriptEnabled: true
       }
     }
+  },
+  build: {
+    // chunk 大小警告的限制
+    chunkSizeWarningLimit: 500,
+    // // 浏览器兼容性  "esnext"|"modules"
+    // target: "modules",
+    // // 输出路径
+    // outDir: "dist",
+    // // 生成静态资源的存放路径
+    // assetsDir: "assets",
+    // 小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。设置为 0 可以完全禁用此项
+    assetsInlineLimit: 4096,
+    // // 构建后是否生成 source map 文件
+    // sourcemap: false,
+    cssCodeSplit: true, // 如果设置为false，整个项目中的所有 CSS 将被提取到一个 CSS 文件中
+    // minify: 'terser',
+    // terserOptions: {
+    //   compress: {
+    //     // warnings: false,
+    //     drop_console: true,  //打包时删除console
+    //     drop_debugger: true, //打包时删除 debugger
+    //     pure_funcs: ['console.log'],
+    //   },
+    //   output: {
+    //     // 去掉注释内容
+    //     comments: true,
+    //   },
+    // },
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+        manualChunks: {
+          // 拆分代码，这个就是分包，配置完后自动按需加载，现在还比不上webpack的splitchunk，不过也能用了。
+          vue: ['vue', 'vue-router', 'pinia']
+          // vant: ['vant']
+          // echarts: ['echarts'],
+        }
+      }
+    },
+    brotliSize: false
   }
 })
---
+++ vitePluginVueDynamicPath.js
@@ -0,0 +1,173 @@
+/**
+ * vite 3.2.4  依赖vite实验性设置 https://cn.vitejs.dev/guide/build.html#advanced-base-options
+ */
+const VITE_PLUGIN_NAME = 'vite-plugin-vue-dynamic-path'
+// 修改异步加载的资源的路径的方法
+
+const resetPublicPathBySource = (
+  dynamicUrl,
+  path = '/sourceConfig.json',
+  sourcePath
+) => `
+function loadJson() {
+  var url = '';
+    if("${dynamicUrl}"){
+      url = "${dynamicUrl}" + "${path}"
+    }else{
+      url =  window.location.origin + "${path}";
+    }
+    var resultUrl = '';
+      // url = 'http://test.21tb.com/sourceConfig.json'
+    var request = new XMLHttpRequest()
+    request.open('get', url)
+    request.send(null)
+    request.onload = function() {
+      if (request.status == 200) {
+        var json = JSON.parse(request.responseText)
+         // var url = json.sourceUrl + '/vue3-project/';
+        var resultUrl = json.sourceUrl + "${sourcePath}";
+        window.__toCdnUrl = resultUrl;
+      }else{
+        var resultUrl = window.location.origin + '${sourcePath || '/'}';
+        window.__toCdnUrl = resultUrl;
+      }
+    }
+    request.onerror = function() {
+      var resultUrl = window.location.origin + '${sourcePath || '/'}';
+      window.__toCdnUrl = resultUrl;
+    }
+}
+loadJson();`
+
+const insertLoadJsonCode = `<script async>${resetPublicPathBySource(
+  '',
+  '/sourceConfig.json',
+  ''
+)}</script>`
+const insertHeadCode = `<script>
+  /**
+   * @description: 
+   * @return {*}
+   * @param {*} fileName  文件名
+   * @param {*} path  文件路径
+   * @param {*} insertTimeout  获取到前缀后多长时间插入
+   * @param {*} retryCount  重试次数
+   */
+  function asyncAppendNode(taglink, path, insertTimeout = 400, retryCount = 5) {
+    var time = null
+    var count = 0
+    // css，js文件地址
+    function createUrl() {
+      return window.__toCdnUrl + path
+    }
+    function insert() {
+      let node
+      // 如果taglink以<script开头，则插入js文件
+      if (taglink.startsWith('<script')) {
+        //替换taglink中的src属性
+        node =
+          taglink.replace(/src=".*?"/, 'src="' + createUrl() + '" />')
+      } else if (taglink.startsWith('<link')) {
+        //替换taglink中的href属性
+        node = taglink.replace(/href=".*?"/, 'href="' + createUrl() + '"')
+      }
+      let doc = new DOMParser().parseFromString(node, 'text/html')
+      document.head.appendChild(doc.documentElement)
+    }
+  
+    function startTask() {
+      count++
+      time && clearTimeout(time)
+      if (window.__toCdnUrl) {
+        setTimeout(() => {
+          console.log((insertTimeout || 200) + 'ms后插入')
+          insert()
+        }, insertTimeout || 200)
+      } else {
+        if (!retryCount || retryCount > count) {
+          time = setTimeout(() => {
+            startTask()
+          }, 200)
+        }
+      }
+    }
+    startTask()
+  }
+  </script>`
+
+function resolveScriptPath(html, viteOptions) {
+  const scriptReg = /<script.*?src="(.*?)".*?>/g
+  let scriptMatch = scriptReg.exec(html)
+  const scriptArr = []
+  while (scriptMatch) {
+    scriptArr.push([scriptMatch[0], scriptMatch[1]])
+    scriptMatch = scriptReg.exec(html)
+  }
+  scriptArr.forEach((item) => {
+    const newScriptWithAppendNode = `<script>asyncAppendNode('${item[0]}','${item[1]}',${viteOptions.loadSourceConfig.insertTimeout},${viteOptions.loadSourceConfig.retryCount});</script>`
+    html = html.replace(item[0], newScriptWithAppendNode)
+  })
+  return html
+}
+
+function resolveLinkPath(html, viteOptions) {
+  const linkReg = /<link.*?href="(.*?)".*?>/g
+  let linkMatch = linkReg.exec(html)
+
+  const linkArr = []
+  while (linkMatch) {
+    linkArr.push([linkMatch[0], linkMatch[1]])
+    linkMatch = linkReg.exec(html)
+  }
+  linkArr.forEach((item) => {
+    const newLinkWithAppendNode = `<script>asyncAppendNode('${item[0]}','${item[1]}',${viteOptions.loadSourceConfig.insertTimeout},${viteOptions.loadSourceConfig.retryCount});</script>`
+    html = html.replace(item[0], newLinkWithAppendNode)
+  })
+  return html
+}
+
+export default function vitePluginVueDynamicPath(options) {
+  const viteOptions = options || {
+    loadSourceConfig: {
+      sourcePath: '/vue3-project/',
+      insertTimeout: 400,
+      retryCount: 5,
+      filePath: '/sourceConfig.json'
+    }
+  }
+  return {
+    name: VITE_PLUGIN_NAME,
+    enforce: 'pre',
+    config(config, { command }) {
+      if (command === 'build') {
+        config.experimental = {
+          renderBuiltUrl(filename, { hostId }) {
+            if (hostId !== 'index.html') {
+              return {
+                runtime: `window.__toCdnUrl(${JSON.stringify(filename)})`
+              }
+            } else {
+              return { relative: true }
+            }
+          }
+        }
+      }
+    },
+    resolveId(id) {
+      if (id === VITE_PLUGIN_NAME) {
+        return id
+      }
+    },
+    transformIndexHtml(html) {
+      html = html.replace(/<head>/, '<head>' + insertHeadCode)
+      html = html.replace(/<head>/, '<head>' + insertLoadJsonCode)
+      html = resolveScriptPath(html, viteOptions)
+      html = resolveLinkPath(html, viteOptions)
+      return html
+    },
+
+    closeBundle() {
+      console.log('Build complete. The dist directory is ready to be deployed.')
+    }
+  }
+}
